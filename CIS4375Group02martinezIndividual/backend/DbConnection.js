71fc5e0829abfea87c603bc573e0a7d95e33828f
Nhat Nguyen <123338202+nhatngCIS@users.noreply.github.com>
2024-04-08 16:47:49 -0500

    Update the DbConnection

--- CIS4375Group02martinezIndividual/backend/DbConnection.js
+++ CIS4375Group02martinezIndividual/backend/DbConnection.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
@@ -6,5 +7,14 @@ const pool = mysql.createPool({
     password: 'P1649320p!',
     database: 'nincompoopDB'
   });
+
+// Test the database connection
+pool.query('SELECT 1', (err, results) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    process.exit(1);
+  }
+  console.log('Connected to the database');
+});
 
   module.exports = pool;
