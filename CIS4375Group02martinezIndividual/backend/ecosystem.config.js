5759a9c5b65b38262422362313b3cf274e66ffcd
Cidiaz1997 <107147828+Cidiaz1997@users.noreply.github.com>
2024-04-09 21:57:35 -0500

    Backend changes

---
+++ CIS4375Group02martinezIndividual/backend/ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    apps : [{
+      name: "app",
+      script: "./app.js",
+      // Optional configuration settings
+      watch: true
+    },{
+      name: "DbConnection",
+      script: "./DbConnection.js",
+      // Optional configuration settings
+      watch: true
+    },{
+      name: "server",
+      script: "./server.js",
+      // Optional configuration settings
+      watch: true
+    }]
+  };
+  
